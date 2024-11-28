9e3114fc83b8729c0f68a8a9b2e1bc23300dee9e
lolozix <lorranasilvadamasceno@gmail.com>
2024-11-28 14:32:28 -0400

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,24 @@
 import React from 'react';
 import PaginaHome from './PaginaHome/PaginaHome';
 import style from "./PaginaDeCadastro/page.module.css"
+import { useEffect } from "react";
+import { parseCookies, setCookie } from "nookies";
+import { useRouter } from "next/navigation";
+
+
 
 
 export default function HomePage() {
+
+  const router = useRouter()
+
+  useEffect(() =>  {
+    const { 'restaurant-token': token } = parseCookies();
+    if (!token) {
+      router.push('/PaginaLogin');
+    }
+  }, [router]);
+
   return (
     <div className={style.btnhover}>
       <PaginaHome />
