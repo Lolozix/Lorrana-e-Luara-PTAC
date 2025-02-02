53562fdf7534b880e6d22aa04b1845328a26dc2c
Lolozix <lorranasilvadamasceno@gmail.com>
2025-02-02 12:07:47 -0400

    commit
    final do video aula 2

--- src/app/Interfaces/reserva.tsx
+++ src/app/Interfaces/reserva.tsx
@@ -8,3 +8,23 @@ interface Reserva {
 }
 
 export default Reserva;
+
+
+//Daqui pra baixo o dele estava na pasta "reservas", arquivo "page.tsx"
+import { redirect } from "next/navigation";
+import { Menu } from "../Components/menu";
+import { ApiURL } from "../utils/config";
+import { cookies } from "next/headers";
+import { fetchUser } from "../utils/auth";
+
+export default function Reservas() {
+     
+    const user = await fechtUser()
+    if(!user) return
+    return (
+        <div className="min-h-screen bg-gray-100 flex flex-col lg:flex-row">
+            
+         <Menu user={user}/>
+        </div>
+    )
+}
