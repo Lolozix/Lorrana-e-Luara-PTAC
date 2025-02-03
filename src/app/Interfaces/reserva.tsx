bcd6dcf7686db421db22ae3a637c70c5f075b5e4
Lolozix <lorranasilvadamasceno@gmail.com>
2025-02-02 22:49:52 -0400

    commit

--- src/app/Interfaces/reserva.tsx
+++ src/app/Interfaces/reserva.tsx
@@ -13,17 +13,14 @@ export default Reserva;
 //Daqui pra baixo o dele estava na pasta "reservas", arquivo "page.tsx"
 import { redirect } from "next/navigation";
 import { Menu } from "../Components/menu";
-import { ApiURL } from "../utils/config";
-import { cookies } from "next/headers";
 import { fetchUser } from "../utils/auth";
 
 export default function Reservas() {
      
-    const user = await fechtUser()
-    if(!user) return
+    const user = await fetchUser()
+    if(!user) redirect(/login)
     return (
         <div className="min-h-screen bg-gray-100 flex flex-col lg:flex-row">
-            
          <Menu user={user}/>
         </div>
     )
